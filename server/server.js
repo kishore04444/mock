7caefe92cae6ddf6a6d7c2c83bdb617233c1c68e
Kishore R <kishoreragu7@gmail.com>
2026-02-02 23:31:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,12 +32,12 @@ app.use(
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
   })
 );
 
-// Handle preflight requests
 app.options('*', cors());
+
+
 
 /* =========================
    MIDDLEWARE
